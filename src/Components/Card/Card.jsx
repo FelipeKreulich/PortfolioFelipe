beca2f99bd524ba9bed1e440f345abf1373c480c
Felipe Kreulich <felipek2002k@gmail.com>
2022-09-20 17:33:32 -0400

    Update Page Services

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Card.css';
 
-const Card = (emoji, heading, detail) => {
+const Card = ({emoji, heading, detail}) => {
   return (
     <div className="card">
       <img src={emoji} alt="Emoji" />
       <span>{heading}</span>
       <span>{detail}</span>
+      <button className="c-button">Learn More</button>
     </div>
   )
 }
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -4,6 +4,7 @@ import HeartEmoji from '../../img/heartemoji.png';
 import Glasses from '../../img/glasses.png';
 import Humble from '../../img/humble.png';
 import Card from '../Card/Card';
+import CV from './felipekreulich.pdf';
 
 const Services = () => {
   return (
@@ -12,23 +13,43 @@ const Services = () => {
       <div className="awesome">
         <span>My Awesome</span>
         <span>Services</span>
-        <span>
-          Lorem Ipsum Dolor Sit Amet Text Of Printing is Simpley
+        <spane>
+        Here are some services I can offer you, just see the options I have available
           <br />
-          Lorem Ipsum Dolor Sit Amet Text Of Printing
-        </span>
-        <button className='button s-button'>Download CV</button>
+          and choose, you can also download my curriculum if you want.
+        </spane>
+        <a href={CV} download>
+          <button className='button s-button'>Download CV</button>
+        </a>
         <div className='blur s-blur1' style={{background: "#ABF1FF94"}}></div>
       </div>
       {/* Right Side */}
       <div className="cards">
-        <div>
+        {/* First Card */}
+        <div style={{left: '14rem'}}>
           <Card 
-            emoji = {HeartEmoji}
-            heading = {'Design'}
-            detail = {"Figma, Sketch, Photoshop, Adobe, Canva"}
+          emoji={HeartEmoji} 
+          heading={'Design'} 
+          detail={"Figma, Sketch, Photoshop, Adobe, Canva"} 
           />
         </div>
+        {/* Second Card */}
+        <div style={{top: '12rem', left: '-4rem'}}>
+          <Card 
+          emoji={Glasses}
+          heading={"Developer"}
+          detail={"HTML, CSS, Javascript, React, Python, NodeJS, Typescript, and Others"}
+          />
+        </div>
+        {/* Three Card */}
+        <div style={{top: '19rem', left: '12rem'}}>
+          <Card 
+          emoji={Humble}
+          heading={"UI/UX"}
+          detail={"Creating a Beautiful Designs for your Enterprise"}
+          />
+        </div>
+        <div className='blur s-blur2' style={{background: 'var(--purple)'}}></div>
       </div>
     </div>
   )
