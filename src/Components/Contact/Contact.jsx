c49f8ab18270efb17e7140b0f54f8ae26921dfbf
Felipe Kreulich <felipek2002k@gmail.com>
2022-09-23 23:41:58 -0400

    Create a Contact Page

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import './Contact.css';
+import { useState } from 'react';
+
+const Contact = () => {
+
+  const form = useRef();
+
+  const [done, setDone] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_q62i0xy', 'template_kwag9h6', form.current, 'd7ngVld5paCGkVcG4')
+      .then((result) => {
+        console.log(result.text);
+        setDone(true);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
+  return (
+    <div className="contact-form">
+      <div className="w-left">
+        <div className="awesome">
+          <span>Entrar em Contato</span>
+          <span>Fale Comigo</span>
+          <div
+            className='blur s-blur1'
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+      </div>
+      <div className="c-right">
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="user_name" className='user' placeholder='Nome' />
+          <input type="email" name='user_email' className='user' placeholder='Email' />
+          <textarea name="message" className='user' placeholder='Mensagem' />
+          <input type="submit" value="Send" className='button' />
+          <span>{done && "Obrigado por me enviar uma mensagem!"}</span>
+          <div
+            className='blur c-blur1'
+            style={{ background: "var(--purple)" }}
+          ></div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
