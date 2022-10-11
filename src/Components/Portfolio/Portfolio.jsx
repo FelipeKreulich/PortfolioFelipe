f24eec30580c4bd7b74e311dfa109919fb1ec195
Felipe Kreulich <felipek2002k@gmail.com>
2022-10-10 22:26:52 -0400

    update png

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Portfolio.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Sidebar from '../../img/sidebar.PNG';
-import Ecommerce from '../../img/ecommerce.PNG';
-import HOC from '../../img/hoc.PNG';
-import MusicApp from '../../img/musicapp.PNG';
+import Sidebar from '../../img/sidebar.png';
+import Ecommerce from '../../img/ecommerce.png';
+import HOC from '../../img/hoc.png';
+import MusicApp from '../../img/musicapp.png';
 import 'swiper/css';
 import { themeContext } from '../../Context';
 import { useContext } from 'react';
