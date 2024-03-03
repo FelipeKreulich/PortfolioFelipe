6f63cea1315fac588c4a12f03640dd25012718e6
FelipeKreulich <felipek2002k@gmail.com>
2024-03-03 23:10:45 +0100

    fixed bug in i18n

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-const { i18n } = require(',/src/i18n');
-
 module.exports = {
   reactStrictMode: true,
-
-  // Adicione estas linhas para configurar a internacionalização
-  rewrites: async () => nextI18NextRewrites(),
-  ...i18n,
 };
