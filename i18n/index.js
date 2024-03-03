38895c935a926e244b667668b83f11a0ae10c728
FelipeKreulich <felipek2002k@gmail.com>
2024-03-04 00:25:07 +0100

    fixed bug in i18n

--- i18n/index.js
+++ i18n/index.js
@@ -1,13 +1,14 @@
-// i18n.js
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import ptBR from './locales/pt/pt-br.json'; // Certifique-se de que o caminho está correto
-import enUS from './locales/en/en-us.json'; // Certifique-se de que o caminho está correto
+// i18n/index.js
+const i18n = require('i18next');
+const { initReactI18next } = require('react-i18next');
+const LanguageDetector = require('i18next-browser-languagedetector');
+
+const PTBR = require('./locales/pt/pt-br.json');
+const ENUS = require('./locales/en/en-us.json');
 
 const resources = {
-  'pt-BR': ptBR,
-  'en-US': enUS,
+  'pt-BR': PTBR,
+  'en-US': ENUS,
 };
 
 i18n
