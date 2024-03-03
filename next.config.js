12055fd4618948e5b785f4aaeac6a24f30470613
FelipeKreulich <felipek2002k@gmail.com>
2024-03-03 23:53:18 +0100

    fixed bug in i18n

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 // next.config.js
-import i18n from './pages/i18n/index.js';
+const { i18n } = require('./pages/i18n/index.js');
 
 module.exports = {
-  i18n: {
-    locales: ['pt-BR', 'en-US'], // Certifique-se de definir as configurações corretas aqui
-    defaultLocale: 'pt-BR', // Defina a localidade padrão
-  },
+  i18n,
   reactStrictMode: true,
 };
