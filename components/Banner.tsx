11ed515e124c177731f97c11bd3e6f1bbe7a4649
FelipeKreulich <felipek2002k@gmail.com>
2024-03-05 00:27:13 +0100

    fixed bugs

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
-import '../pages/i18n';
 
 const Banner = () => {
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,7 +17,7 @@ import { MdOutlineClose } from "react-icons/md";
 import { motion } from "framer-motion";
 import 'react-toastify/dist/ReactToastify.css';
 import { useTranslation } from 'react-i18next';
-import '../pages/i18n';
+import '../utils/i18n/index.js';
 
 const getStoredLanguage = () => localStorage.getItem('i18nextLng') || 'pt-BR';
 function LanguageSwitcher() {
